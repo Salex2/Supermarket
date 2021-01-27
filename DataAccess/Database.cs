9e0ebece9df9190ef8fe3ea7915b235bbcd3c21a
Alex S <alex.com>
2021-01-27 17:44:53 +0200

    UI

---
+++ DataAccess/Database.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess
+{
+    public class Database
+    {
+
+        private readonly List<Product> Products = new List<Product>
+        {
+
+            new Product { ProductName = "Detergent", Price = 20 },
+            new Product { ProductName = "Oranges", Price = 42 },
+            new Product { ProductName = "Juice", Price = 10 },
+            new Product { ProductName = "Meat", Price = 19 },
+            new Product { ProductName = "Beer", Price = 25 },
+        };
+       
+
+        
+        public List<Product> GetProducts()
+        {
+            return Products;
+        }
+    }
+}
--- Supermarket/ViewModels/SupermarketViewModel.cs
+++ Supermarket/ViewModels/SupermarketViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +9,22 @@ using System.Threading.Tasks;
 
 namespace Supermarket.ViewModels
 {
+    
     //Screen allows for more control over opening and closing; 
     //EXP: If you want to close a form without saving you can get an Event to ask you if you are sure about exiting without saving
-    public class SupermarketViewModel : Screen
+    public class SupermarketViewModel : Conductor<object>
     {
+        //Bindable collection- handles things like alerting us when something has changed on the list. Update on the VIEW
+        public BindableCollection<Product> Products { get; set; }
+      
+        public SupermarketViewModel()
+        {
+            Database db = new Database();
+
+            Products = new BindableCollection<Product>(db.GetProducts());
+
+
+        }
 
     }
 }
