f7051bb1807def87bef148e09790e32079ce12c0
Alex S <alex.com>
2021-01-27 12:39:14 +0200

    first commit

---
+++ Supermarket/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using Supermarket.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Supermarket
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        //instead of MainWindow.XAML our launch point is SuperMarketViewModel
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<SupermarketViewModel>();
+        }
+    }
+}
---
+++ Supermarket/ViewModels/SupermarketViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Supermarket.ViewModels
+{
+    //Screen allows for more control over opening and closing; 
+    //EXP: If you want to close a form without saving you can get an Event to ask you if you are sure about exiting without saving
+    public class SupermarketViewModel : Screen
+    {
+
+    }
+}
